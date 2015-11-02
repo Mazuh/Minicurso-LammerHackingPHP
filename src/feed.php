76c2f10d741a1c4152e449f4b5fdaa409e6ef075
= <marcell-mz@hotmail.com>
2015-11-02 20:51:01 -0300

    Template do feed

---
+++ src/feed.php
@@ -0,0 +1,46 @@
+<link rel="stylesheet" type="text/css" href="css/custom/feed.css"/>
+<div class="container">
+<div class="blog-header">
+    <h1 class="blog-title">Feed de notícias</h1>
+    <p class="lead blog-description">Estas são as 5 publicações mais recentes de seus seguidores.</p>
+</div>
+
+<div class="row">
+
+    <div class="col-sm-8 blog-main">
+
+        <div class="blog-post">
+            <h2 class="blog-post-title">Hello, world</h2>
+            <p class="blog-post-meta">Publicado em por <a href="#">Mazuh</a> (10-10-10 10:10) |
+                Tem <a href="#">2 comentário(s)</a></p>
+
+            <div class="conteudo">
+                Wadda wadda.
+            </div>
+            
+        </div><!-- /.blog-post -->
+        
+        <!--
+        <nav>
+            <ul class="pager">
+                <li><a href="#">Previous</a></li>
+                <li><a href="#">Next</a></li>
+            </ul>
+        </nav>
+        -->
+        
+    </div><!-- /.blog-main -->
+
+    <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
+        <div class="sidebar-module sidebar-module-inset">
+            <h4>Networking</h4>
+            <ol class="list-unstyled">
+                <li><a href="https://github.com/Mazuh/" target="_blank">GitHub</a></li>
+                <li><a href="https://www.facebook.com/marcell.guilherme" target="_blank">Facebook</a></li>
+            </ol>
+        </div>
+    </div><!-- /.blog-sidebar -->
+
+</div><!-- /.row -->
+
+</div><!-- /.container -->
