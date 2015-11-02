548b325a6244e191a883bd4945a2c91718c51d6b
= <marcell-mz@hotmail.com>
2015-11-02 16:38:45 -0300

    Template de login

--- src/index.php
+++ src/index.php
@@ -1,36 +1,75 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-    
+
     <head>
-        
+
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="Simplório e frágil sistema em que usuários podem publicar artigos prórpios e ler ou escrever comentários nas publicações alheias.">
+        <meta name="description" content="Simplório e frágil sistema em que usuários podem publicar artigos própios e ler ou escrever comentários nas publicações alheias.">
         <meta name="author" content="Mazuh">
 
         <title>The Hackbook</title>
 
         <!-- Bootstrap core CSS -->
         <link href="css/bootstrap.min.css" rel="stylesheet">
-        
+
+        <style type="text/css">
+            /* Move down content because we have a fixed navbar that is 50px tall */
+            body {
+                padding-top: 50px;
+                padding-bottom: 20px;
+            }
+            
+            footer{
+                background-color: lightgray;
+            }
+        </style>
+
     </head>
 
     <body>
-        
+
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">The Hackbook</a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <form class="navbar-form navbar-right">
+                        <div class="form-group">
+                            <input type="search" placeholder="Buscar hacker" class="form-control">
+                        </div>
+                        <button type="submit" class="btn btn-success">Buscar</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+
         <?php
         // inclui o template solicitado
-        
+
         $pagina = "login";
-            
+
         if (isset($_REQUEST["t"])){
             $pagina = $_REQUEST["t"];
         }
-        
+
         include($pagina . ".php");
-        
+
         ?>
-        
+
+        <br/>
+        <footer class="container text-center">
+            <p>(c) 2015 - Desenvolvido por <a href="https://github.com/Mazuh/Minicurso-HackingPHP">Mazuh</a></p>
+        </footer>
+
     </body>
-    
+
 </html>
--- src/login.php
+++ src/login.php
@@ -1,23 +1,57 @@
 
 <link href="css/custom/login.css" rel="stylesheet">
 
+<div class="jumbotron">
+    <div class="container">
+        <h1>The Hackbook<span id="piscante" class="">_</span></h1>
+        <h2>Sistema livre para hackers acessarem e escreverem suas publicações e comentários.</h2>
+    </div>
+</div>
 
 <div class="container">
 
-    <form class="form-signin" action="script/logar.php" method="post">
+    <div class="row">
 
-        <h1>The Hackbook<span id="piscante" class="">_</span></h1>
+        <div class="col-md-5">
 
-        <h2 class="form-signin-heading">Entre agora!</h2>
-        <label for="inputEmail" class="sr-only">E-mail</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="E-mail" required autofocus>
-        <label for="inputPassword" class="sr-only">Senha</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Senha" required>
+            <form class="form-signin" action="script/logar.php" method="post">
+                <h2 class="form-signin-heading">Entre agora!</h2>
+                <label for="inputLoginUsuario" class="sr-only">Usuário</label>
+                <input type="text" id="inputLoginUsuario" class="form-control" placeholder="Usuário" required autofocus>
+                <label for="inputPassword" class="sr-only">Senha</label>
+                <input type="password" id="inputPassword" class="form-control" placeholder="Senha" required>
 
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Acessar</button>
-        <p>Ou cadastre-se <a href="index.php?t=cadastro">aqui</a>.</p>
-        
-    </form>
+                <br/>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Acessar</button>
+
+            </form>
+
+        </div>
+
+        <div class="col-md-7">
+
+            <form class="form-signin" action="script/logar.php" method="post">
+                <h2 class="form-signin-heading">Abra uma conta!</h2>
+                <label for="inputEmail" class="sr-only">E-mail</label>
+                <input type="email" id="inputEmail" class="form-control" placeholder="E-mail" required>
+                
+                <label for="inputPassword" class="sr-only">Senha</label>
+                <input type="password" id="inputPassword" class="form-control" placeholder="Senha" required>
+                
+                <label for="inputRePassword" class="sr-only">Repetir senha</label>
+                <input type="password" id="inputRePassword" class="form-control" placeholder="Repetir senha" required>
+                
+                <label for="inputLoginUsuario" class="sr-only">Usuário</label>
+                <input type="text" id="inputUsuario" class="form-control" placeholder="Usuário" required>
+
+                <br/>
+                <button class="btn btn-lg btn-warning btn-block" type="submit">Cadastrar</button>
+
+            </form>
+
+        </div>
+
+    </div>
 
 </div>
 
