7aaa65bb935751cab659806e9d87218d7f6d3778
= <marcell-mz@hotmail.com>
2015-11-02 19:33:12 -0300

    Script de login

--- src/script/logar.php
+++ src/script/logar.php
@@ -1,5 +1,30 @@
 <?php
 
+session_start();
 
+include("conexao.php");
+
+if (isset($_REQUEST["nick"]) && isset($_REQUEST["senha"])){
+    
+    $nick = $_REQUEST["nick"];
+    $senha = hash("sha256", $_REQUEST["senha"]); // criptografia
+    
+    // busca / select
+    $sql = "select * from usuario where nick='$nick' and senha='$senha'";
+    $res = mysqli_query($link, $sql);
+    
+    if (mysqli_num_rows($res) > 0){
+        
+        $usuario = mysqli_fetch_array($res);
+        
+        // registra e redireciona
+        $_SESSION["usuario"] = $usuario["id"];
+        header("location: ../?t=feed");
+        
+    } else{
+        die("Acesso negado.");
+    }
+    
+}
 
 ?>
