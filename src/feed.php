1bbe966326d11f7c92aa3da0e50ab669eb9ee6d9
= <marcell-mz@hotmail.com>
2015-11-02 22:56:01 -0300

    Listagem do feed implementada

--- src/feed.php
+++ src/feed.php
@@ -9,17 +9,44 @@
 
     <div class="col-sm-8 blog-main">
 
+        <?php
+        
+        include("script/conexao.php");
+        $usuario = $_SESSION["usuario"];
+        
+        // PRIMEIRO INNER JOIN DA MINHA VIDA, QUE FUMO!!!!!!
+        $sql = "select postagem.*, seguimento.* from postagem inner join seguimento on postagem.autor = seguimento.seguido where seguimento.seguidor = ".$usuario["id"];
+        
+        $res = mysqli_query($link, $sql);
+        $qtd = 0;
+        
+        // listagem de postagens
+        while (($postagem = mysqli_fetch_array($res)) && $qtd <= 5){
+            $qtd++;
+            
+            // conta comentários
+            $sql = "select * from comentario where postagem = ".$postagem["id"];
+            $qtdComentarios = mysqli_num_rows(mysqli_query($link, $sql));
+            // verifica nome do autor
+            $sql = "select * from usuario where id = ".$postagem["autor"];
+            $nomeAutor = mysqli_fetch_array(mysqli_query($link, $sql))["nick"];
+            
+        ?>
+        
         <div class="blog-post">
-            <h2 class="blog-post-title">Hello, world</h2>
-            <p class="blog-post-meta">Publicado em por <a href="#">Mazuh</a> (10-10-10 10:10) |
-                Tem <a href="#">2 comentário(s)</a></p>
+            <h2 class="blog-post-title"><?php echo $postagem["titulo"]; ?></h2>
+            <p class="blog-post-meta">Publicado por <a href="#"><?php echo $nomeAutor; ?></a>
+                | <?php echo $postagem["datahora"]; ?> <br/>
+                Recebeu <a href="#"><?php echo $qtdComentarios; ?> comentário(s)</a></p>
 
             <div class="conteudo">
-                Wadda wadda.
+                <?php echo $postagem["conteudo"]; ?>
             </div>
             
         </div><!-- /.blog-post -->
-        
+        <?php
+        }
+        ?>
         <!--
         <nav>
             <ul class="pager">
